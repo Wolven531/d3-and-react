7f17b47f541678ef4dfc019546179e91be33f3ac
wolven531 <anthony.williams.cs@gmail.com>
2021-04-28 21:16:08 -0600

    Add useInterval custom hook

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,5 @@
+import React, { useState, useEffect, useRef } from 'react'
+
 export const generateDataset = () =>
 	Array(10).fill(0)
 		.map(() =>
@@ -6,3 +8,24 @@ export const generateDataset = () =>
 				Math.random() * 33 + 10, // y-coordinate
 			])
 		)
+
+// inspired by https://overreacted.io/making-setinterval-declarative-with-react-hooks/
+export function useInterval(callback: Function, delay: number) {
+	const savedCallback = useRef<any>()
+
+	// remember the latest callback
+	useEffect(() => {
+		savedCallback.current = callback
+	}, [callback])
+
+	// set up the interval
+	useEffect(() => {
+		function tick() {
+			savedCallback.current()
+		}
+		if (delay !== null) {
+			let id = setInterval(tick, delay)
+			return () => clearInterval(id)
+		}
+	}, [delay])
+}
