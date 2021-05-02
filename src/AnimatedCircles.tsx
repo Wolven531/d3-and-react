c507e6afc79175b214a04c1fcdbc387d1331a5d0
wolven531 <anthony.williams.cs@gmail.com>
2021-05-02 16:56:29 -0600

    Add AnimatedCircles

---
+++ src/AnimatedCircles.tsx
@@ -0,0 +1,48 @@
+import { MutableRefObject, useEffect, useRef, useState } from 'react'
+import * as d3 from 'd3'
+import { generateDataset, useInterval } from './utils'
+
+const AnimatedCircles = () => {
+	const ref: MutableRefObject<SVGSVGElement> = useRef<MutableRefObject<SVGSVGElement>>() as unknown as MutableRefObject<SVGSVGElement>
+	const generateCircles = () => {
+		return generateDataset()
+	}
+	const [visibleCircles, setVisibleCircles] = useState(generateCircles())
+
+	useInterval(() => {
+		setVisibleCircles(generateCircles())
+	}, 2000)
+
+	useEffect(() => {
+		const svgElement = d3.select(ref.current)
+		svgElement.selectAll('circle')
+			.data(visibleCircles)
+			.join(
+				(enter) => enter.append('circle')
+					.attr('cx', (d) => d[0] * 15 + 10)
+					.attr('cy', 10)
+					.attr('r', 0)
+					.attr('fill', 'cornflowerblue')
+					.call(
+						(enter) => enter.transition()
+							.duration(1200)
+							.attr('cy', 10)
+							.attr('r', 6)
+							.style('opacity', 1)
+					),
+				(update) => update.attr('fill', 'lightgrey'),
+				(exit) => exit.attr('fill', 'tomato')
+					.call(
+						(exit) => exit.transition()
+							.duration(1200)
+							.attr('r', 0)
+							.style('opacity', 0)
+							.remove()
+					)
+			)
+	}, [visibleCircles])
+
+	return <svg viewBox="0 0 100 20" ref={ref} />
+}
+
+export default AnimatedCircles
