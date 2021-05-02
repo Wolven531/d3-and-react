7a4a0f8140df5ade699e06d5053d24d857765aed
wolven531 <anthony.williams.cs@gmail.com>
2021-05-01 21:44:30 -0600

    Remove unnecessary code

--- src/Circles.tsx
+++ src/Circles.tsx
@@ -1,21 +1,9 @@
-import { MutableRefObject, useEffect, useRef, useState } from 'react'
+import { useState } from 'react'
 import { generateDataset, useInterval } from './utils'
-import * as d3 from 'd3'
 
 const Circles = () => {
-	// const ref: MutableRefObject<SVGSVGElement> = useRef<SVGSVGElement>() as MutableRefObject<SVGSVGElement>
 	const updateTimeSecs = 1000
 	const [dataset, setDataset] = useState(generateDataset())
-
-	// useEffect(() => {
-		// const svgElement = d3.select(ref.current)
-		// svgElement.selectAll('circle')
-		// 	.data(dataset)
-		// 	.join('circle')
-		// 	.attr('cx', (d) => d[0])
-		// 	.attr('cy', (d) => d[1])
-		// 	.attr('r', 3)
-	// }, [dataset])
 
 	useInterval(() => {
 		const newDataset = generateDataset()
