0bbf7a6ea05f3ccef73235d0765759753a7cbec8
wolven531 <anthony.williams.cs@gmail.com>
2021-05-08 22:41:13 -0600

    Update unit tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,10 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from 'react'
+import { render, screen } from '@testing-library/react'
+import App from './App'
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+	const comp = render(<App />)
+	expect(comp.getByText(/D3 and React/)).toBeInTheDocument()
+	// const linkElement = screen.getByText(/learn react/i)
+	// expect(linkElement).toBeInTheDocument()
+})
