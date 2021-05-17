1db998e6478aa895e048c2d7625a0a8ea235a517
wolven531 <anthony.williams.cs@gmail.com>
2021-05-16 20:47:03 -0600

    Add boilerplate for buttons

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,8 @@
+import Button from '@material-ui/core/Button'
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import React from 'react'
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import ReactDOM from 'react-dom'
 import Circle from './Circle'
 import Circles from './Circles'
 import Svg from './Svg'
@@ -14,6 +18,17 @@ function App() {
 				<Svg></Svg>
 				<Circle></Circle>
 				<Circles></Circles>
+			</section>
+			<section>
+				<Button variant="contained">
+					Show SVG
+				</Button>
+				<Button variant="contained" color="primary">
+					Show Solo Circle
+				</Button>
+				<Button variant="contained" color="secondary">
+					Show Circles
+				</Button>
 			</section>
 		</div>
 	)
