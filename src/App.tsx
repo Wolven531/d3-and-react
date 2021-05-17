d74693b62c23807dc9df5ebbce6f046d2359b432
wolven531 <anthony.williams.cs@gmail.com>
2021-05-16 20:57:05 -0600

    Use Styled Components API

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import Button from '@material-ui/core/Button'
+import { styled } from '@material-ui/core/styles'
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import React, { useState } from 'react'
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
@@ -10,6 +11,16 @@ import './App.css'
 
 function App() {
 	const [currentDisplayIndex, setCurrentDisplayIndex] = useState(-1)
+	const CustomStyledButton = styled(Button)({
+		background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+		// border: 0,
+		// borderRadius: 3,
+		boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+		color: 'white',
+		margin: '10px',
+		// height: 48,
+		// padding: '0 30px',
+	})
 
 	return (
 		<div>
@@ -22,15 +33,15 @@ function App() {
 				{currentDisplayIndex === 2 && <Circles></Circles>}
 			</section>
 			<section>
-				<Button variant="contained" onClick={() => { setCurrentDisplayIndex(0) }}>
+				<CustomStyledButton variant="contained" onClick={() => { setCurrentDisplayIndex(0) }}>
 					Show SVG
-				</Button>
-				<Button variant="contained" color="primary" onClick={() => { setCurrentDisplayIndex(1) }}>
+				</CustomStyledButton>
+				<CustomStyledButton variant="contained" color="primary" onClick={() => { setCurrentDisplayIndex(1) }}>
 					Show Solo Circle
-				</Button>
-				<Button variant="contained" color="secondary" onClick={() => { setCurrentDisplayIndex(2) }}>
+				</CustomStyledButton>
+				<CustomStyledButton variant="contained" color="secondary" onClick={() => { setCurrentDisplayIndex(2) }}>
 					Show Circles
-				</Button>
+				</CustomStyledButton>
 			</section>
 		</div>
 	)
