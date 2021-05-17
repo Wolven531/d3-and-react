f9ebdf9fde93502bba129d0011658ece4290be22
wolven531 <anthony.williams.cs@gmail.com>
2021-05-16 20:49:19 -0600

    Hook up conditional render

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import Button from '@material-ui/core/Button'
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-import React from 'react'
+import React, { useState } from 'react'
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import ReactDOM from 'react-dom'
 import Circle from './Circle'
@@ -9,15 +9,17 @@ import Svg from './Svg'
 import './App.css'
 
 function App() {
+	const [currentDisplayIndex, setCurrentDisplayIndex] = useState(-1)
+
 	return (
 		<div>
 			<header>
 				<h3>D3 and React</h3>
 			</header>
 			<section>
-				<Svg></Svg>
-				<Circle></Circle>
-				<Circles></Circles>
+				{currentDisplayIndex === 0 && <Svg></Svg>}
+				{currentDisplayIndex === 1 && <Circle></Circle>}
+				{currentDisplayIndex === 2 && <Circles></Circles>}
 			</section>
 			<section>
 				<Button variant="contained">
