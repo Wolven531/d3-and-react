670e93207ce74197912ab355d57c409b117a28ef
wolven531 <anthony.williams.cs@gmail.com>
2021-05-17 21:16:00 -0600

    Update unit test structure - App

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,10 +1,20 @@
 import React from 'react'
-import { render, screen } from '@testing-library/react'
+import { render, RenderResult, screen } from '@testing-library/react'
 import App from './App'
 
-test('renders learn react link', () => {
-	const comp = render(<App />)
-	expect(comp.getByText(/D3 and React/)).toBeInTheDocument()
-	// const linkElement = screen.getByText(/learn react/i)
-	// expect(linkElement).toBeInTheDocument()
+describe('App component', () => {
+	let comp: RenderResult
+
+	beforeEach(() => {
+		comp = render(<App />)
+	})
+
+	it('renders header properly', () => {
+		// expect(comp.getByText(/D3 and React/)).toBeInTheDocument()
+		// expect(screen.getByText(/D3 and React/)).toBeInTheDocument()
+
+		const headerResult = comp.container.querySelectorAll('header')
+		expect(headerResult).toHaveLength(1)
+		expect(headerResult[0].textContent).toBe('D3 and React')
+	})
 })
